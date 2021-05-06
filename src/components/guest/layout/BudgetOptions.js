a5543221e47863888a55b2484e977cfd3b978db7
Deleon Francis <dxfrancis.coding@gmail.com>
2021-05-06 16:12:41 -0400

    save btn now done btn.

--- src/components/guest/layout/BudgetOptions.js
+++ src/components/guest/layout/BudgetOptions.js
@@ -15,41 +15,8 @@ function BudgetOptions() {
     <div className="">
     <div className="center-align">
     <a onClick={handleReset} href="#!" className="waves-effect waves-light btn-small"><i className="material-icons left red-text" style={{fontSize:"25px", marginRight:"3px"}}>restart_alt</i>reset</a>
-    <a style={{marginLeft:"15px"}} href="#!" className="waves-effect waves-light btn-small"><i className="material-icons left" style={{fontSize:"25px", marginRight:"3px"}}>save</i>save</a>
+    <a style={{marginLeft:"15px"}} href="#!" className="waves-effect waves-light btn-small"><i className="material-icons left" style={{fontSize:"25px", marginRight:"3px"}}>done</i>done</a>
     </div>
-
-      {/* <div className="col">
-        <a onClick={handleReset} href="#!" className="">
-          <i
-            style={{ fontSize: "30px" }}
-            className="material-icons center-align red-text"
-          >
-            restart_alt
-          </i>
-          <p
-            style={{ fontSize: "10px", marginTop: "0%", marginBottom: "0%" }}
-            className="center-align black-text"
-          >
-            Reset
-          </p>
-        </a>
-      </div>
-      <div style={{ paddingRight: "0%" }} className="col">
-        <a href="#!" className="">
-          <i
-            style={{ fontSize: "30px" }}
-            className="material-icons center-align green-text lighten-3"
-          >
-            save
-          </i>
-          <p
-            style={{ fontSize: "10px", marginTop: "0%", marginBottom: "0%" }}
-            className="center-align black-text"
-          >
-            Save
-          </p>
-        </a>
-      </div> */}
     </div>
   );
 }
