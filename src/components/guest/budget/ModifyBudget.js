6665e5dc8645c5be61b469ee8e58950815c8673c
Deleon Francis <dxfrancis.coding@gmail.com>
2021-05-06 13:18:24 -0400

    close btn on modify budget.

--- src/components/guest/budget/ModifyBudget.js
+++ src/components/guest/budget/ModifyBudget.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { connect, useDispatch } from "react-redux";
+import { setShowModifyBudget } from "../../../actions/mainActions";
 
-function ModifyBudget() {
+function ModifyBudget({guestMain:{currency,balance}}) {
+
+const dispatch = useDispatch()
+
+const handleClose = () => {
+dispatch(setShowModifyBudget(false))
+}
+
   return (
-    <div className="row">
+    <div className="row z-depth-2 yellow lighten-5" style={{padding:"20px"}}>
       <div className="input-field">
+      <h4 className="prefix teal-text center-align">
+            {currency}
+          </h4>
         <input id="icon_prefix" type="text" class="validate" />
         <label for="icon_prefix">Modify Budget</label>
         <span className="helper-text " data-error="wrong" data-success="right">
@@ -11,15 +23,34 @@ function ModifyBudget() {
         </span>
       </div>
       <div className="center-align">
-        <a href="#!" style={{marginRight:"15px"}}>
-          <i style={{fontSize:"35px"}} className="material-icons prefix green-text">add</i>
+        <a href="#!" style={{ marginRight: "15px" }}>
+          <i
+            style={{ fontSize: "35px" }}
+            className="material-icons prefix green-text"
+          >
+            add
+          </i>
         </a>
         <a href="#!">
-          <i style={{fontSize:"35px"}} className="material-icons prefix red-text">remove</i>
+          <i
+            style={{ fontSize: "35px" }}
+            className="material-icons prefix red-text"
+          >
+            remove
+          </i>
         </a>
+      </div>
+      <div className="grey-text">
+        {balance >=0 && <a onClick={handleClose} className="grey-text" href="#!" style={{ marginRight: "15px",fontStyle:"italic" , textDecoration:"underline" }}>
+          close
+        </a>}
       </div>
     </div>
   );
 }
 
-export default ModifyBudget;
+const mapStateToProps = (state) => ({
+    guestMain: state.guestMain,
+  });
+
+export default connect(mapStateToProps)(ModifyBudget);
