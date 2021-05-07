7475dd14f0efae2cb8e9d1275e88047419ed78b6
Deleon Francis <dxfrancis.coding@gmail.com>
2021-05-07 07:25:45 -0400

    fixed negative balance display on BudgetItem.

--- src/components/guest/layout/BudgetItem.js
+++ src/components/guest/layout/BudgetItem.js
@@ -9,7 +9,7 @@ function BudgetItem({budget, theme}) {
         <tr className="hoverable" style={theme==="dark" ? {borderColor:"teal"} : {}}>
             <td><a className="modal-trigger" style={theme==="dark" ? {fontSize:"18px", color: "white"} : {fontSize:"18px", color: "black"}} href="#editBudgetModal">{title}</a></td>
             <td className="center-align">{currency}{budgetAmount}</td>
-            <td className={balance < 0 ? "red-text center-align" : "green-text center-align"}>{currency}{balance}</td>
+            {balance>=0?<td className="green-text center-align">{currency}{balance}</td>:<td className="red-text center-align">{balance}{currency}</td>}
             <td>{expenses.map((exp)=> <div key={exp.id}>{exp.expName} {" "}({budget.currency}{exp.expAmount})</div>)}</td>
             <td className="center-align"><Moment format="MMMM Do, YYYY">{dateCreated}</Moment></td>
             <td className="center-align"> <a href="#editBudgetModal" className="modal-trigger"><i className="material-icons teal-text">edit</i></a>  <a href="#deleteBudgetModal" className="modal-trigger"><i className="material-icons red-text">delete</i></a></td>
