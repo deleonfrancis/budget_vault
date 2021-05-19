c6a156b4057c7d0a24b462ebc374027c0531dc84
Deleon Francis <dxfrancis.coding@gmail.com>
2021-05-19 12:47:47 -0400

    styed about modal and trigger.

--- src/components/guest/layout/AboutTrigger.js
+++ src/components/guest/layout/AboutTrigger.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function AboutTrigger() {
-    return (
-        <div>
-            <a href="#aboutModal" className="modal-trigger">about</a>
-        </div>
-    )
+  return (
+    <div className="center-align">
+      <a
+        href="#aboutModal"
+        className="modal-trigger"
+        style={{ fontStyle: "italic", fontSize: "18px", color: "#9e9e9e" }}
+      >
+        about
+      </a>
+    </div>
+  );
 }
 
-export default AboutTrigger
+export default AboutTrigger;
--- src/pages/GuestHome.js
+++ src/pages/GuestHome.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import AboutTrigger from '../components/guest/layout/AboutTrigger'
-import Budgets from '../components/guest/layout/Budgets'
-import ComposeBudgetBtn from '../components/guest/layout/ComposeBudgetBtn'
-import DeleteAllBudgetsBtn from '../components/guest/layout/DeleteAllBudgetsBtn'
-import GuestFilter from '../components/guest/layout/GuestFilter'
+import React from "react";
+import AboutTrigger from "../components/guest/layout/AboutTrigger";
+import Budgets from "../components/guest/layout/Budgets";
+import ComposeBudgetBtn from "../components/guest/layout/ComposeBudgetBtn";
+import DeleteAllBudgetsBtn from "../components/guest/layout/DeleteAllBudgetsBtn";
+import GuestFilter from "../components/guest/layout/GuestFilter";
 
-function GuestHome({theme}) {
-    return (
-        <div>
-            <GuestFilter theme={theme} />
-            <ComposeBudgetBtn />
-            <Budgets theme={theme} />
-            <DeleteAllBudgetsBtn />
-            <AboutTrigger />
-        </div>
-    )
+function GuestHome({ theme }) {
+  return (
+    <div>
+      <AboutTrigger theme={theme} />
+      <GuestFilter theme={theme} />
+      <ComposeBudgetBtn />
+      <Budgets theme={theme} />
+      <DeleteAllBudgetsBtn />
+    </div>
+  );
 }
 
-export default GuestHome
+export default GuestHome;
