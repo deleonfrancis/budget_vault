8c8b6e73935e1a24a2809a4751f6b85a79ddb6e0
Deleon Francis <dxfrancis.coding@gmail.com>
2021-06-08 10:19:05 -0400

    connected app to mongoDB database.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
-
 const app = express();
+
+const connectDB = require('./config/db')
+
+
+// Connect Database
+connectDB()
 
 app.get("/", (req, res) =>
   res.json({ msg: "welcome to the Budget Vault api..." })
