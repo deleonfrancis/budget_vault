e9624573f8858c799852e2329b60929e3a65f7f5
Deleon Francis <dxfrancis.coding@gmail.com>
2021-06-08 14:00:54 -0400

    able to update and delete budget in the database.

--- server/routes/budgets.js
+++ server/routes/budgets.js
@@ -52,13 +52,69 @@ router.post("/", auth, async (req, res) => {
 });
 
 // update budget. private access
-router.put("/:id", (req, res) => {
-  res.send("update budget");
+router.put("/:id", auth, async (req, res) => {
+  const {
+    title,
+    currency,
+    budgetAmount,
+    balance,
+    expenses,
+    note,
+    dateUpdated,
+    dateCreated,
+  } = req.body;
+
+  // Build a budget object
+  const budgetFields = {};
+
+  if (title) budgetFields.title = title;
+  if (currency) budgetFields.currency = currency;
+  if (budgetAmount) budgetFields.budgetAmount = budgetAmount;
+  if (balance) budgetFields.balance = balance;
+  if (expenses) budgetFields.expenses = expenses;
+  if (note) budgetFields.note = note;
+  if (dateUpdated) budgetFields.dateUpdated = dateUpdated;
+  if (dateCreated) budgetFields.dateCreated = dateCreated;
+
+  try {
+    let budget = await Budget.findById(req.params.id);
+    if (!budget) return res.status(404).json({ msg: "Budget not found" });
+
+    // make sure user owns the budget
+    if (budget.user.toString() !== req.user.id)
+      return res.status(401).json({ msg: "Not Authorized." });
+
+    //   Update the Budget
+    budget = await Budget.findByIdAndUpdate(
+      req.params.id,
+      { $set: budgetFields },
+      { new: true }
+    );
+
+    res.json(budget)
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error.");
+  }
 });
 
 // auth user and get token. private access
-router.delete("/:id", (req, res) => {
-  res.send("delete budget");
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    let budget = await Budget.findById(req.params.id);
+    if (!budget) return res.status(404).json({ msg: "Note not found." });
+
+    // make sure user owns the budget
+    if (budget.user.toString() !== req.user.id)
+      return res.status(401).json({ msg: "Not Authorized." });
+
+    //   Delete the budget
+    budget = await Budget.findByIdAndRemove(req.params.id);
+    res.json({ msg: "Note removed." });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("server error.");
+  }
 });
 
 module.exports = router;
