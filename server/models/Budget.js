7ba611fa7205555aa1f3d52551647b92bc559d7b
Deleon Francis <dxfrancis.coding@gmail.com>
2021-06-08 13:37:29 -0400

    able to add a budget to the database.

--- server/models/Budget.js
+++ server/models/Budget.js
@@ -1,55 +1,62 @@
 const mongoose = require("mongoose");
 
 const BudgetSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users", // name of the collection you want to refer to
+  },
 
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "users", // name of the collection you want to refer to
-    },
-
-    title: {
+  title: {
+    type: String,
+    required: true,
+  },
+  currency: {
+    type: String,
+    required: true,
+  },
+  budgetAmount: {
+    type: Number,
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+  expenses: [
+    {
+      id: {
         type: String,
-        required: true,
+        required: false,
       },
-    currency: {
+      expName: {
         type: String,
-        required: true,
+        required: false,
       },
-    budgetAmount: {
+      expAmount: {
         type: Number,
-        required: true,
+        required: false,
       },
-    balance:{
-        type: Number,
-        required: true,
+      expNote: {
+        type: String,
+        required: false,
       },
-    expenses: [
-      {
-        id: {
-            type: String,
-            required: false,
-          },
-        expName: {
-            type: String,
-            required: false,
-          },
-        expAmount: {
-            type: Number,
-            required: false,
-          },
-        date: {
-            type: Date,
-            default: Date.now(),
-          }
-      }
-    ],
-    dateUpdated: {
-        type: Date,
-      },
-    dateCreated: {
+      date: {
         type: Date,
         default: Date.now(),
-      }
-})
+      },
+    },
+  ],
+  note: {
+    type: String,
+    required: false,
+  },
+  dateUpdated: {
+    type: Date,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
-module.exports = mongoose.model("budget", BudgetSchema)
+module.exports = mongoose.model("budget", BudgetSchema);
--- server/routes/budgets.js
+++ server/routes/budgets.js
@@ -1,36 +1,64 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const { body, validationResult } = require("express-validator");
 
-const auth = require('../middleware/auth')
+const auth = require("../middleware/auth");
 
 const User = require("../models/User");
 const Budget = require("../models/Budget");
 
-
 // get user's budgets. Private access
-router.get('/', auth, async (req, res) => {
-try {
-    const budgets = await Budget.find({user:req.user.id}).sort({date:-1})
-    res.json(budgets)
-} catch (error) {
-    console.error(error.message)
+router.get("/", auth, async (req, res) => {
+  try {
+    const budgets = await Budget.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(budgets);
+  } catch (error) {
+    console.error(error.message);
     res.status(500).send("Server Error.");
-}})
+  }
+});
 
 // add a new budget private access
-router.post('/', (req, res) => {
-    res.send('add budget')
-})
+router.post("/", auth, async (req, res) => {
+  const {
+    title,
+    currency,
+    budgetAmount,
+    balance,
+    expenses,
+    note,
+    dateUpdated,
+    dateCreated,
+  } = req.body;
+  try {
+    const newBudget = new Budget({
+      user: req.user.id,
+      title,
+      currency,
+      budgetAmount,
+      balance,
+      expenses,
+      note,
+      dateUpdated,
+      dateCreated,
+    });
+
+    const budget = await newBudget.save();
+    res.json(budget);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error.");
+  }
+});
 
 // update budget. private access
-router.put('/:id', (req, res) => {
-    res.send('update budget')
-})
+router.put("/:id", (req, res) => {
+  res.send("update budget");
+});
 
 // auth user and get token. private access
-router.delete('/:id', (req, res) => {
-    res.send('delete budget')
-})
+router.delete("/:id", (req, res) => {
+  res.send("delete budget");
+});
 
 module.exports = router;
