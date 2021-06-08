a10cdcf769a558af006cbd2033e4f7662b8df347
Deleon Francis <dxfrancis.coding@gmail.com>
2021-06-08 10:52:58 -0400

    able to save a user to the database.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs')
 
 const User = require("../models/User");
 
@@ -17,14 +18,55 @@ router.post(
   body("password", "Password of 6 or more characters is required.").isLength({
     min: 6,
   }),
-  (req, res) => {
+  async (req, res) => {
     // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        // returns a 'Bad Request' 400 code and an array of errors.
-        return res.status(400).json({ errors: errors.array() });
+      // returns a 'Bad Request' 400 status and an array of errors.
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // destructor firstName, lastName, email, password from the request.body
+    const { firstName, lastName, email, password } = req.body;
+
+    try {
+      // check to see if the email already exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        // bad request with msg
+        return (
+          res
+            .status(400)
+            // .json({ msg: `A user with the email ${email} already exist.` });
+            .json({ msg: "User already exists" })
+        );
       }
+      // creates a new user
+      user = new User({
+        firstName,
+        lastName,
+        email,
+        password,
+      });
+
+      // expression 'salt' that will encrypt the password
+      const salt = await bcrypt.genSalt(10);
+
+      // hashes the password and assign it to the user object
+      user.password = await bcrypt.hash(password, salt);
+
+      //   Saves user to the database
+      await user.save();
+
+      res.send('user saved')
+
+    } catch (error) {
+        console.error(error.message)
+        // return a 500 status: server error.
+        res.status(500).send("Server Error");
+    }
 
     res.send(req.body);
   }
