d479375479339a4b55621ab44579f574660a5c64
Deleon Francis <dxfrancis.coding@gmail.com>
2021-06-09 17:31:01 -0400

    reorganized types and created auth types.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import guestBudgetReducer from "./guestBudgetReducer";
 import guestMainReducer from "./guestMainReducer";
 import userBudgetReducer from "./userBudgetReducer";
 import userMainReducer from "./userMainReducer";
+import authReducer from "./authReducer";
 
 export default combineReducers({
   guestBudget: guestBudgetReducer,
   guestMain: guestMainReducer,
   userBudget: userBudgetReducer,
   userMain: userMainReducer,
+  authReducer: authReducer, 
 });
